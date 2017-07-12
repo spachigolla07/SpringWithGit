6260613a5e88de4fb14ac12eb359845dea46b3aa
nisum <nisum@nisums-MacBook-Pro-5.local>
2017-07-12 05:53:57 -0700

    Initial Commit

---
+++ src/main/java/com/nisum/SpringBootWithGit/SpringBootWithGitApplication.java
@@ -0,0 +1,12 @@
+package com.nisum.SpringBootWithGit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithGitApplication.class, args);
+	}
+}
