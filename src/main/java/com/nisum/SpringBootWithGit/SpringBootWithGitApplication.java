065b110704bdab57cb74c330a39946ad1e44f209
nisum <nisum@nisums-MacBook-Pro-5.local>
2017-07-12 06:44:54 -0700

    final commit Squashed commit of the following:
    commit 861f70c1648cdcfb9bf409b8e1f4a1ebe1656a2f
    Merge: e58e7fa 7e634a0
    Author: nisum <nisum@nisums-MacBook-Pro-5.local>
    Date:   Wed Jul 12 06:43:39 2017 -0700
    
        resolved conflicts
        Merge branch 'master' into new-branch
    
    commit e58e7faae236ac3ba954bdd68532bafbc2216994
    Author: nisum <nisum@nisums-MacBook-Pro-5.local>
    Date:   Wed Jul 12 06:21:46 2017 -0700
    
        commit from sasank

--- src/main/java/com/nisum/SpringBootWithGit/SpringBootWithGitApplication.java
+++ src/main/java/com/nisum/SpringBootWithGit/SpringBootWithGitApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootWithGitApplication {
 
 	public static void main(String[] args) {
-		System.out.println("hello from bharath");
+		System.out.println("from sasank");
 		SpringApplication.run(SpringBootWithGitApplication.class, args);
 	}
 }
