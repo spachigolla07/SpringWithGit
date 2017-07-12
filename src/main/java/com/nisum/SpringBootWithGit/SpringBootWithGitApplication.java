e58e7faae236ac3ba954bdd68532bafbc2216994
nisum <nisum@nisums-MacBook-Pro-5.local>
2017-07-12 06:21:46 -0700

    commit from sasank

--- src/main/java/com/nisum/SpringBootWithGit/SpringBootWithGitApplication.java
+++ src/main/java/com/nisum/SpringBootWithGit/SpringBootWithGitApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootWithGitApplication {
 
 	public static void main(String[] args) {
+		System.out.println("from sasank");
 		SpringApplication.run(SpringBootWithGitApplication.class, args);
 	}
 }
